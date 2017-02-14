95b794a2240358812f1b5badd118abb0635a3780
Andrew Dalton and Jasun Feddema <andrew@>
2017-02-13 21:00:17 -0800

    add submit button, and experiment with ways to make information display.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function()  {
-  $("#blanks form").submit(function(event)  {
+  $("#survey form").submit(function(event)  {
     var surveyUser = $("input#user").val();
     var dob = $("#born").val();
     var favoriteColor = $("#color").val());
     var favoriteMusic = $("input:radio[name=genre]:checked").val();
     var beverage = $("#beverage").val();
+
+    $(".output").text(surveyUser);
+
+
+    $(".output").alert(surveyUser);
+
+    event.preventDefault();
   });
 });
